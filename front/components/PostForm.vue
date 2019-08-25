69a51940e0c78901494b9b457afbe8971afacfda
KIMINJOONG <ijboym216@gmail.com>
2019-08-25 19:44:51 +0900

    add post and delete post with dummy data

--- front/components/PostForm.vue
+++ front/components/PostForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card>
+    <v-card style="margin-bottom: 20px">
         <v-container>
             <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
                 <v-textarea 
@@ -14,10 +14,9 @@
                     :rules="[v => !!v.trim() || '내용을 입력하세요.']"
                     @input="onChangeTextarea"
                 />
-
+                <v-btn type="submit" color="green" absolute right>짹짹</v-btn>
+                <v-btn>이미지 업로드</v-btn>
             </v-form>
-            <v-btn type="submit" color="green" absolute right>짹짹</v-btn>
-            <v-btn>이미지 업로드</v-btn>
         </v-container>
     </v-card>
 </template>
@@ -28,6 +27,7 @@ import { mapState } from 'vuex';
 export default {
     data(){
         return {
+            valid: false,
             hideDetails: true,
             successMessages: '',
             success: false,
@@ -56,6 +56,7 @@ export default {
                     id: Date.now(),
                     createdAt: Date.now()
                 }).then(() => {
+                    this.content = '';
                     this.hideDetails = false;
                     this.success = true;
                     this.successMessages = '게시글 등록 성공!';
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -2,12 +2,7 @@
     <v-container>
         <post-form v-if="me" />
         <div>
-            <post-card />
-            <post-card />
-            <post-card />
-            <post-card />
-            <post-card />
-            <post-card />
+            <post-card v-for="p in mainPosts" :key="p.id" :post="p" />
         </div>
     </v-container>
 </template>
@@ -24,6 +19,9 @@ export default {
         me() {
             return this.$store.state.users.me;
         },
+        mainPosts() {
+            return this.$store.state.posts.mainPosts;
+        }
     }
 }
 </script>
