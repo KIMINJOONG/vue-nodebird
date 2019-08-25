f6869cafc3d663adf096d53ce9710b97acca5279
KIMINJOONG <ijboym216@gmail.com>
2019-08-25 19:30:17 +0900

    make post form

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <v-container>
+        <post-form v-if="me" />
         <div>
             <post-card />
             <post-card />
@@ -12,9 +13,17 @@
 </template>
 <script>
 import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
 export default {
     components: {
         PostCard,
+        PostForm,
+        
+    },
+    computed: {
+        me() {
+            return this.$store.state.users.me;
+        },
     }
 }
 </script>
