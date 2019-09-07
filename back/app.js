97e87d3b039e4c523e0eab212741398f63413be6
KIMINJOONG <ijboym216@gmail.com>
2019-09-07 20:59:41 +0900

    send data to server

--- back/app.js
+++ back/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
    res.send('안녕 백엔드');
 });
 
+app.post('/user', (req, res) => {
+    
+})
 app.listen(3085, () => {
     console.log(`http://localhost:${3085}`)
 });
