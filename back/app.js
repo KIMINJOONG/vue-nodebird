f54e86a6c6f80bdd3c0f91b9febc916c1447e9f4
KIMINJOONG <ijboym216@gmail.com>
2019-09-08 23:51:58 +0900

    sequelize model and join user

--- back/app.js
+++ back/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const db = require('./models');
 const app = express();
+
+db.sequelize.sync();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -9,8 +11,20 @@ app.get('/', (req, res) => {
    res.send('안녕 백엔드');
 });
 
-app.post('/user', (req, res) => {
+app.post('/user', async (req, res, next) => {
+    try {
+        const newUser = await db.User.create({
+            email: req.body.email,
+            password: req.body.password,
+            nickname: req.body.nickname,
+        }); //HTTP STATUS CODE
+        res.status(201).json(newUser);
+    }catch(err) {
+        console.log(err);
+        next(err);
+    }
     
+
 })
 app.listen(3085, () => {
     console.log(`http://localhost:${3085}`)
