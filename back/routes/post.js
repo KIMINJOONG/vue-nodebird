8757dec5f416b68ddf55d1abab0cf45db6ebe490
KIMINJOONG <ijboym216@gmail.com>
2019-09-19 23:37:17 +0900

    add like, remove like in server

--- back/routes/post.js
+++ back/routes/post.js
@@ -192,6 +192,34 @@ router.post('/:id/retweet', async (req, res, next) => {
     }
 });
 
+router.post('/:id/like', isLoggedIn, async (req, res, next) => {
+    try{
+        const post = await db.Post.findOne({ where: { id: req.params.id } });
+        if(!post) {
+            return res.status(404).send('포스트가 존재하지 않습니다.');
+        }
+        await post.addLiker(req.user.id);
+        return res.json({ userId: req.user.id });
+    }catch(error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.delete('/:id/like', isLoggedIn, async (req, res, next) => {
+    try{
+        const post = await db.Post.findOne({ where: { id: req.params.id } });
+        if(!post) {
+            return res.status(404).send('포스트가 존재하지 않습니다.');
+        }
+        await post.removeLiker(req.user.id);
+        return res.json({ userId: req.user.id });
+    }catch(error) {
+        console.error(error);
+        next(error);
+    }
+});
+
 
 
 module.exports = router;
